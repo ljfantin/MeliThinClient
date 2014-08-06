60faa1ae95469554c460bda716b0f6c3eec51a16
Leandro Fantin <leandro.fantin@mercadolibre.com>
2014-08-06 12:55:29 -0300

    Initial Commit

---
+++ MeliThinClient/MTCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTCAppDelegate.h
+//  MeliThinClient
+//
+//  Created by Leandro Fantin on 06/08/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeliThinClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeliThinClient
+//
+//  Created by Leandro Fantin on 06/08/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTCAppDelegate class]));
+    }
+}
