9545e5ab829d85674bc7fe4c8843b2485efd1dc8
Leandro Fantin <lea@oktubre-2.local>
2014-08-14 08:22:51 -0300

    currency

---
+++ MeliThinClient/MTCCurrencyDto.m
@@ -0,0 +1,13 @@
+//
+//  MTCCurrencyDto.m
+//  MeliThinClient
+//
+//  Created by Leandro Fantin on 14/08/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import "MTCCurrencyDto.h"
+
+@implementation MTCCurrencyDto
+
+@end
